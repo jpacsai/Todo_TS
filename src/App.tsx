0e8d472eee86978e383ef40ad67cb73099fbd77e
unknown <judit.pacsai@gmail.com>
2019-02-04 23:58:11 +0100

    add basic input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,33 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Shopping List</h1>
         </header>
+        <form className="App-form">
+          <section className="App-input">
+            <span>To buy:</span>
+            <input type="text" name="item" value=""/>
+          </section>
+          <section className="App-input">
+            <span>Amount:</span>
+            <input type="number" name="amount" value=""/>
+            <section>
+            <span>Unit:</span>
+              <select>
+                <option value="kg">kg</option>
+                <option value="dkg">dkg</option>
+                <option value="litre">litre</option>
+                <option value="piece">piece</option>
+                <option value="box">box</option>
+                <option value="bottle">bottle</option>
+                <option value="can">can</option>
+                <option value="packet">packet</option>
+                <option value="bunch">bunch</option>
+              </select>
+            </section>
+          </section>
+          <input type="submit" value="Submit"/>
+        </form> 
       </div>
     );
   }
