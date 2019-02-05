31b49f2c42535a09929ba5205a5135e02a2de97d
jpacsai <judit.pacsai@gmail.com>
2019-02-05 11:21:17 +0100

    simplify input form

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,7 @@ class App extends Component {
           <h1>Shopping List</h1>
         </header>
         <form className="App-form">
-          <section className="App-input">
-            <span>To buy:</span>
-            <input type="text" name="item" value=""/>
-          </section>
-          <section className="App-input">
-            <span>Amount:</span>
-            <input type="number" name="amount" value=""/>
-            <section>
-            <span>Unit:</span>
-              <select>
-                <option value="kg">kg</option>
-                <option value="dkg">dkg</option>
-                <option value="litre">litre</option>
-                <option value="piece">piece</option>
-                <option value="box">box</option>
-                <option value="bottle">bottle</option>
-                <option value="can">can</option>
-                <option value="packet">packet</option>
-                <option value="bunch">bunch</option>
-              </select>
-            </section>
-          </section>
+          <input type="text" name="item" value=""/>
           <input type="submit" value="Submit"/>
         </form> 
       </div>
